ee991aa664b87c03bc585fb9da903d63f2d510b9
okywi <m.laupheimer@protonmail.com>
2024-06-19 11:40:20 +0200

    first commit

---
+++ html/src/de/school/indiegame/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package de.school.indiegame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import de.school.indiegame.Main;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Main();
+        }
+}
