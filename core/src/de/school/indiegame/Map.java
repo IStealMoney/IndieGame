80570d7430ca68c5d89aa5a9b676c17795351a79
okywi <m.laupheimer@protonmail.com>
2024-06-19 18:31:01 +0200

    wie machen wir die map?

---
+++ core/src/de/school/indiegame/Map.java
@@ -0,0 +1,15 @@
+package de.school.indiegame;
+
+public class Map {
+
+    int[][] map;
+
+    Map() {
+        map = load_map("map-1");
+    }
+
+    public static int[][] load_map(String mapName) {
+        // Keine Ahnung wie genau ihr das machen wollt
+        return null;
+    }
+}
