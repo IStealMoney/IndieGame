184e49a9b4441d6fa6fd17270902a8b1b6b5f6e0
okywi <m.laupheimer@protonmail.com>
2024-06-20 08:22:13 +0200

    changed tile size

--- core/src/de/school/indiegame/Main.java
+++ core/src/de/school/indiegame/Main.java
@@ -11,7 +11,7 @@ public class Main extends ApplicationAdapter {
 	public static int[] GAME_SIZE = {400, 225};
 	public static int MULTIPLIER = 4;
 	public static int[] SCREEN_SIZE = {GAME_SIZE[0] * MULTIPLIER, GAME_SIZE[1] * MULTIPLIER};
-	public static int TILE_SIZE = 20;
+	public static int TILE_SIZE = 16;
 	public static Map MAP;
 
 	SpriteBatch batch;
