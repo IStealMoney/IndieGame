f45083fb25acf09bb3faa70c1938ec020e999432
Lenja Aicher <IStealMoney@users.noreply.github.com>
2024-07-11 23:27:44 +0200

    created MoneySystem.java for interactive purposes with NPC to sell items

---
+++ core/src/de/school/indiegame/MoneySystem.java
@@ -0,0 +1,34 @@
+package de.school.indiegame;
+
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+import static de.school.indiegame.Main.font;
+import static de.school.indiegame.Main.gameScreen;
+
+public class MoneySystem {
+    public static Sprite moneyValue;
+    static int currentMoney = 0;
+    static String currency = "Bob-Coins";
+
+    //interactive old-Bob-Skin to sell items?
+
+    MoneySystem() {
+
+    }
+
+    public void createCustomer() {
+
+    }
+
+    public void isInRange() {
+
+    }
+
+    public void updateCurrentMoney() {
+
+    }
+}
